e3c2575a9270cc60888089ada35e09c6edd73a57
changyunyeong <sally0109277@naver.com>
2024-06-23 17:34:46 +0900

    7주차 bug: 이미지 랜더링

--- Kotlin/MyApplication/udemy_android_template_bottomnavi/app/src/main/java/com/example/flo/LockerVPAdapter.kt
+++ Kotlin/MyApplication/udemy_android_template_bottomnavi/app/src/main/java/com/example/flo/LockerVPAdapter.kt
@@ -4,12 +4,12 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class LockerVPAdapter(fragment: Fragment) : FragmentStateAdapter(fragment){
-    override fun getItemCount(): Int = 3
+    override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> SavedSongFragment()
-            1 -> MusicFileFragment()
+//            1 -> MusicFileFragment()
             else -> SavedAlbumFragment()
         }
     }
--- Kotlin/MyApplication/udemy_android_template_bottomnavi/app/src/main/java/com/example/flo/SavedSongFragment.kt
+++ Kotlin/MyApplication/udemy_android_template_bottomnavi/app/src/main/java/com/example/flo/SavedSongFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.flo.databinding.FragmentLockerSavedsongBinding
 
 public class SavedSongFragment : Fragment(){
 
     lateinit var binding: FragmentLockerSavedsongBinding
+    lateinit var songDB: SongDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +19,28 @@ public class SavedSongFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLockerSavedsongBinding.inflate(inflater, container, false)
+        songDB = SongDatabase.getInstance(requireContext())!!
+
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.lockerSavedSongRecyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        val songRVAdapter = SavedSongRVAdapter()
+        songRVAdapter.setMyItemClickListener(object : SavedSongRVAdapter.MyItemClickListener {
+            override fun onRemoveSong(songId: Int) {
+                songDB.songDao().updateIsLikeById(false, songId)
+            }
+        })
+
+        binding.lockerSavedSongRecyclerView.adapter = songRVAdapter
+        songRVAdapter.addSongs(songDB.songDao().getLikedSongs(true) as ArrayList<Song>)
     }
 }
